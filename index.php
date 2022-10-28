02bdc9d00d03fb6e69eeb0d3dc214125f2bfe4b2
Miguel <47696065+MegRov@users.noreply.github.com>
2022-10-28 15:06:01 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <title>Catalogo </title>
+</head>
+<body>
+  <!-- Navbar con categorias -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<img src="Imagenes/Logo.jpg" width="100" height="60" class="d-inline-block align-top rounded" alt="">
+  <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+    <div class="navbar-nav ">
+      <a class="nav-item nav-link btn btn-outline-info" href="#">Descuentos</a>
+      <a class="nav-item nav-link btn btn-outline-info" href="#">Papeleria</a>
+      <a class="nav-item nav-link btn btn-outline-info" href="#">Hogar y uso diario</a>
+      <a class="nav-item nav-link btn btn-outline-info" href="#">Belleza y cuidado personal</a>
+    </div>
+  </div>
+</nav>
+
+<!--Galeria de imagenes-->
+<div class="container bg-success">
+  <div class="card-group">
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto1.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto2.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto3.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    </div>
+  </div>
+  <div class="card-group">
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto1.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto2.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto3.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    </div>
+  </div>
+  <div class="card-group">
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto1.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto2.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto3.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    </div>
+  </div>
+  <div class="card-group">
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto1.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto2.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    <div class="card border border-success">
+      <img class="card-img-top p-7  rounded " src="Imagenes/Producto3.JPG" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">Esferos de foquita 🥰.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Precio por mayor $50 usd</small>
+        <a class=" btn btn-outline-info" href="#">Añadir al carrito</a>
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
+
+</body>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+</html>
